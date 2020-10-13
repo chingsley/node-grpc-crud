b3bf38aa52ceb7f1d998b22fdc6f13a469397b75
chingsley <eneja.kc@gmail.com>
2020-10-13 04:00:25 +0100

    remove the console that logs connection credentials and add .env.sample

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,12 +1,6 @@
 require("dotenv").config();
 const path = require("path");
 
-console.log({
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
 module.exports = {
   development: {
     client: "postgresql",
